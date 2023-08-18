df67d5926ceeb61f1d82111e6b268e4d7c4454c2
SoSuddenly <122160671+SoSuddenly@users.noreply.github.com>
2023-08-18 13:44:05 +0300

    коментарі

--- lib/api_service_memory.dart
+++ lib/api_service_memory.dart
@@ -7,18 +7,17 @@ import 'internet_utils.dart';
 
 class ApiService extends GetxController {
   static const String apiUrl = 'https://reqres.in/api';
-  static const int totalOfflinePages = 0;
+
+  static const int totalOfflinePages = 1;
 
   static Future<void> saveOfflineUsers(List<UserModel> users, int page) async {
     final prefs = await SharedPreferences.getInstance();
 
-    // Отримання раніше збереженого JSON-рядка зі списком користувачів
     final existingDataJson = prefs.getString('offlineData');
 
-    // Створення об'єкта для існуючих офлайн даних
     final existingData = <int, List<UserModel>>{};
 
-    // Перевірка, чи є раніше збережений JSON-рядок
+    // Парсинг раніше збережених даних
     if (existingDataJson != null) {
       final existingDataMap =
           json.decode(existingDataJson) as Map<String, dynamic>;
@@ -31,33 +30,34 @@ class ApiService extends GetxController {
       });
     }
 
-    // Збереження списку користувачів для конкретної сторінки
     existingData[page] = users;
 
-    // Перетворення об'єкта в Map зі збереженими даними
+    // Перетворення даних в Map і збереження в SharedPreferences
     final offlineDataJson = existingData.map((key, value) {
       return MapEntry(
           key.toString(), value.map((user) => user.toJson()).toList());
     });
-
-    // Збереження даних у SharedPreferences
     prefs.setString('offlineData', json.encode(offlineDataJson));
 
-    // Додайте логування для виведення інформації про збережені дані
-    print('Saved users for page $page: ${users.length} users');
+    // Логування збережених даних
+    // print('Saved users for page $page: ${users.length} users');
   }
 
+  // Реактивні змінні для статусу отримання додаткової інформації
   RxBool _isFetchingAdditionalInfo = false.obs;
   bool get isFetchingAdditionalInfo => _isFetchingAdditionalInfo.value;
 
+  // Реактивні змінні для перевірки наявності додаткової інформації
   RxBool _hasAdditionalInfo = false.obs;
   bool get hasAdditionalInfo => _hasAdditionalInfo.value;
 
+  // Реактивний Map для додаткової інформації про користувача
   RxMap<String, dynamic> _additionalUserInfo = <String, dynamic>{}.obs;
-  Map<String, dynamic> get additionalUserInfo =>
-      _additionalUserInfo.value; // Зміна
+  Map<String, dynamic> get additionalUserInfo => _additionalUserInfo.value;
 
+  // Метод для отримання додаткової інформації про користувача
   Future<void> fetchAdditionalUserInfo(int userId) async {
+    // Встановлюємо прапорець отримання додаткової інформації
     _isFetchingAdditionalInfo.value = true;
     try {
       final response = await http.get(Uri.parse('$apiUrl/users/$userId'));
@@ -66,13 +66,12 @@ class ApiService extends GetxController {
       if (data != null) {
         _additionalUserInfo.value.assignAll(data);
         _hasAdditionalInfo.value = true;
-        // Зберегти додаткову інформацію в SharedPreferences
+        // Збереження додаткової інформації локально
         await saveAdditionalUserInfoLocally(userId, data);
       } else {
         _hasAdditionalInfo.value = false;
       }
     } catch (e) {
-      // При помилці, спробуйте отримати інформацію з локального сховища
       final localInfo = await fetchAdditionalUserInfoLocally(userId);
       if (localInfo != null) {
         _additionalUserInfo.value.assignAll(localInfo);
@@ -86,12 +85,14 @@ class ApiService extends GetxController {
     }
   }
 
+  // Метод для збереження додаткової інформації локально
   Future<void> saveAdditionalUserInfoLocally(
       int userId, Map<String, dynamic> data) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString('additionalUserInfo_$userId', json.encode(data));
   }
 
+  // Метод для отримання додаткової інформації локально
   Future<Map<String, dynamic>?> fetchAdditionalUserInfoLocally(
       int userId) async {
     final prefs = await SharedPreferences.getInstance();
@@ -102,60 +103,58 @@ class ApiService extends GetxController {
     return null;
   }
 
+  // Метод для отримання користувачів з можливістю відкату до офлайн даних
   static Future<List<Map<String, dynamic>>> fetchUsersWithFallback(
       int page) async {
+    // Список для збереження даних
     List<Map<String, dynamic>> usersData = [];
 
     try {
+      // Перевірка наявності Інтернет-з'єднання
       final hasInternet = await InternetUtils.hasInternetConnection();
 
       if (hasInternet) {
+        // Отримання даних з API
         print('Fetching users online for page $page');
         final response = await http.get(Uri.parse('$apiUrl/users?page=$page'));
         final data = json.decode(response.body)['data'] as List<dynamic>?;
-
         if (data != null) {
+          // Збереження офлайн даних
           List<UserModel> users = List<UserModel>.from(
               data.map((item) => UserModel.fromJson(item)));
-
-          // Збереження користувачів для конкретної сторінки
           saveOfflineUsers(users, page);
-
-          // Конвертувати список UserModel у список JSON-представлень користувачів (Map)
+          // Перетворення в JSON-представлення
           usersData = users.map((user) => user.toJson()).toList();
         }
       } else {
+        // Отримання даних з локального сховища
         print('Fetching users offline for page $page');
-        // Отримання оновлених даних з локального сховища
         final offlineUsers = await fetchOfflineData(page);
-
         usersData = offlineUsers.map((user) => user.toJson()).toList();
         print('Fetched users from local storage: ${offlineUsers.length}');
       }
     } catch (e) {
-      // Тут відбувається таймаут або помилка при з'єднанні
+      // Обробка помилок під час отримання даних
       print('Error or timeout occurred: $e');
     }
 
     if (usersData.isEmpty) {
-      // Отримання оновлених даних з локального сховища
+      // Отримання даних з локального сховища
       final offlineUsers = await fetchOfflineData(page);
-
       usersData = offlineUsers.map((user) => user.toJson()).toList();
     }
 
     return usersData;
   }
 
+  // Метод для отримання офлайн даних для певної сторінки
   static Future<List<UserModel>> fetchOfflineData(int page) async {
     final prefs = await SharedPreferences.getInstance();
     final offlineDataJson = prefs.getString('offlineData');
-
     if (offlineDataJson != null) {
       final offlineDataMap =
           json.decode(offlineDataJson) as Map<String, dynamic>;
       final userListData = offlineDataMap[page.toString()] as List<dynamic>;
-
       return userListData
           .map<UserModel>((item) => UserModel.fromJson(item))
           .toList();
@@ -164,16 +163,18 @@ class ApiService extends GetxController {
     }
   }
 
+  // Метод для отримання загальної кількості сторінок
   static Future<int> fetchTotalPages() async {
     try {
+      // Отримання даних з API
       final response = await http.get(Uri.parse('$apiUrl/users'));
       final data = json.decode(response.body) as Map<String, dynamic>?;
       if (data != null) {
         return data['total_pages'] ?? 1;
       } else {
+        // Відновлення збережених офлайн даних
         final prefs = await SharedPreferences.getInstance();
         final offlineDataJson = prefs.getString('offlineData');
-
         if (offlineDataJson != null) {
           final offlineDataMap =
               json.decode(offlineDataJson) as Map<String, dynamic>;
@@ -186,10 +187,10 @@ class ApiService extends GetxController {
         }
       }
     } catch (e) {
+      // Обробка помилок під час отримання загальної кількості сторінок
       print('Error fetching total pages: $e');
       final prefs = await SharedPreferences.getInstance();
       final offlineDataJson = prefs.getString('offlineData');
-
       if (offlineDataJson != null) {
         final offlineDataMap =
             json.decode(offlineDataJson) as Map<String, dynamic>;

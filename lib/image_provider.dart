3d2d1091b756ed2b24e8c7dbdcee2452aa5fc882
SoSuddenly <122160671+SoSuddenly@users.noreply.github.com>
2023-08-22 10:31:21 +0300

    комент

--- lib/image_provider.dart
+++ lib/image_provider.dart
@@ -2,9 +2,8 @@ import 'package:flutter/widgets.dart';
 
 ImageProvider getImageProvider(String avatarUrl, bool hasInternet) {
   if (hasInternet) {
-    return NetworkImage(avatarUrl); // Зображення з Інтернету
+    return NetworkImage(avatarUrl);
   } else {
-    return const AssetImage(
-        'images/no_internet_icon.png'); // Зображення з ресурсів
+    return const AssetImage('images/no_internet_icon.png');
   }
 }
